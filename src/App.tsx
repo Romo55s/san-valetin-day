4c8d3dc29f4380383f9659e1d4773e23b42408f2
Romo55s <pp.tony777@gmail.com>
2025-02-09 01:57:49 -0600

    Adding the background and 3 components for the visuals

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Stack from "./components/Stack";
+import TextPressure from "./components/TextPressure";
+import Aurora from "./components/Aurora";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const images = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=500&auto=format",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1452626212852-811d58933cae?q=80&w=500&auto=format",
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="aurora-background">
+        <Aurora colorStops={["#FF1493", "#FF69B4", "#FFC0CB"]} speed={0.5} />
       </div>
-      <h1 className='text-blue-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="content flex justify-center items-center">
+        <div className="container flex flex-col gap-10 text-center p-4 md:p-10">
+          <div>
+            <div className="relative h-12 md:h-48 lg:h-10 w-full max-w-4xl mx-auto">
+              <TextPressure
+                text="Happy Valentines Day!"
+                flex={true}
+                alpha={false}
+                stroke={false}
+                width={true}
+                weight={true}
+                italic={true}
+                textColor="#ffffff"
+                strokeColor="#ff0000"
+                minFontSize={42}
+                maxFontSize={92}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center items-center relative">
+            <Stack
+              randomRotation={true}
+              sensitivity={180}
+              sendToBackOnClick={false}
+              cardDimensions={{ width: 300, height: 300 }}
+              cardsData={images}
+            />
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
