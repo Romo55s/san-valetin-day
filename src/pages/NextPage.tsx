e610481073e65df0fbcaa223d6ba853317903dc1
Romo55s <pp.tony777@gmail.com>
2025-02-11 23:23:28 -0600

    Adding text-center to the last page

--- src/pages/NextPage.tsx
+++ src/pages/NextPage.tsx
@@ -12,7 +12,7 @@ const NextPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full p-4">
       <img src={macaco1} alt="Next" className="w-64 h-64 rounded-lg mb-8" />
-    <h1 className="text-2xl">Love you chula, happy Valentine's Day!</h1>
+    <h1 className="text-2xl text-center">Love you chula, happy Valentine's Day!</h1>
       <button
         className="mt-4 bg-black text-white py-2 px-4 md:py-4 md:px-8 text-xl md:text-2xl rounded-lg cursor-pointer shiny-background border-gray-600 border-2 transition duration-300 ease-in-out transform hover:bg-rose-800 hover:scale-105 active:bg-rose-800 active:scale-105"
         onClick={handleBackToStart}
